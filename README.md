a544a1f392ebaa9607962e204a6da1f60ba9cf89
Chris Coleman-Smith <cec24@phy.duke.edu>
2013-06-02 21:00:25 -0400

    added a little script to make a test archive of random bin files

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ ccs
 
 An educational project which aims to explore and recreate the zip
 archive format and compression scheme. 
+
+## First Goal
+
+Open an archive, list the contents, recreating `unzip -l`
+
+## Test Data
+
+Created random files with dd:
+
+`dd if=/dev/random of=./test-data/foo count=10`
+
+Create a test archive with `make-test-arc.sh`
---
+++ make-test-arc.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+## zip up the contents of test-data
+filelist=(foo bar baz)
+
+if [ ! -d ./test-data ]; then
+		echo "making test-data folder"
+		mkdir ./test-data
+fi
+
+for file in $filelist; do
+		if [ ! -e ./test-data/$file ]; then 
+				echo "cannot find: $file"
+				dd if=/dev/random of=./test-data/$file count=10
+		fi
+done
+
+echo "Making test archive"
+zip -v ./test-arc.zip ./test-data/*
